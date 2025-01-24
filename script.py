fea39e24d15195404aae98afa5e4f521fd0f87c9
yandaki <boayves@gmail.com>
2025-01-24 17:12:32 +0000

    Add image preprocessing and enhance text extraction visualization

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ import pytesseract
 from pytesseract import Output
 import os
 from PIL import Image
+from paddleocr import PaddleOCR
 
 # Chemin vers l'exécutable Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -53,12 +54,26 @@ def detect_regions(image_path):
     return detections, image
 
 
+def preprocess_image(image):
+    # Convertir en niveaux de gris
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Appliquer une binarisation
+    _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    # Appliquer un filtre pour réduire le bruit
+    filtered = cv2.medianBlur(binary, 3)
+    
+    return filtered
+
 def extract_text(image_path):
     # Détecter les régions
     detections, image = detect_regions(image_path)
     
     # Stocker les résultats dans un dictionnaire
     extracted_data = {}
+    overlay = image.copy()
+    alpha = 0.1
     
     for detection in detections:
         # Découper chaque région détectée
@@ -89,10 +104,19 @@ def extract_text(image_path):
             extracted_data[label] = [] # Crée une liste pour chaque nouveau label
         extracted_data[label].append(detection_data)
         
+        cv2.rectangle(overlay, (xmin, ymin), (xmax, ymax), (0, 255, 0), -1)  # Dessiner un rectangle rempli
+
+        # Ajouter le libellé de la classe prédite en dessous de la zone colorée
+        text_position = (xmin, ymax + 20)
+        font_scale = 0.3  # Taille du texte réduite
+        cv2.putText(image, label, text_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 0), 2)
+        
         # Dessiner les rectangles sur l'image
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-        cv2.putText(image, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        # cv2.rectangle(image,(xmin, ymin),(xmax, ymax),(0, 255, 0),2)
+        # cv2.putText(image, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
     
+    # Appliquer le surlignage avec transparence
+    cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, image)
     # Enregistrer l'image avec les rectangles dessinés
     output_image_path = os.path.splitext(image_path)[0] + "_annotated.jpg"
     cv2.imwrite(output_image_path, image)
